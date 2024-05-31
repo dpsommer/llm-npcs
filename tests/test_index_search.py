8ff3bfe89221e8826bef0e4bd8565d024f801b13
Duncan Sommerville <duncan.sommerville@gmail.com>
2024-05-31 14:32:19 -0400

    Add test for multiple entity search

--- tests/test_index_search.py
+++ tests/test_index_search.py
@@ -29,6 +29,12 @@ def memories():
             "entities": ["Silver Fox"],
             "sentiment_polarity": 0,
         },
+        {
+            "npc": "John Doe",
+            "memory": "A mug of ale costs 4 copper pieces at the Silver Fox.",
+            "entities": ["ale", "Silver Fox"],
+            "sentiment_polarity": 0,
+        }
     ]
 
 
@@ -54,9 +60,14 @@ def file_based_index(memories, clean_test_subtree):
 
 def test_search_by_npc_name(in_memory_index):
     results = search_memories(index=in_memory_index, search_string='npc:"John Doe"')
-    assert len(results) == 3
+    assert len(results) == 4
 
 
 def test_search_by_entity_name(in_memory_index):
     results = search_memories(index=in_memory_index, search_string='entities:"ale"')
+    assert len(results) == 2
+
+
+def test_search_by_multiple_entities(in_memory_index):
+    results = search_memories(index=in_memory_index, search_string='entities:"ale" entities:"Silver Fox"')
     assert len(results) == 1
