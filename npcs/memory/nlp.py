10bf7c6cb8ab3dcf78a35bbdcc2f09979e9e5881
Duncan Sommerville <duncan.sommerville@gmail.com>
2024-06-01 14:58:28 -0400

    Migrate from coreferee to fastcoref
    coreferee requires spacy<3.6, which has some compatibility
    issues with our langchain dependencies that rely on Pydantic
    v2. Instead, switch to fastcoref and refactor the NLP
    interface.
    
    Instead of providing each NLP pipeline component as a
    separate function, we now just surface a wrapper around the
    pipeline Doc result. This means we no longer need multiple
    pipeline runs to retrieve values, and changes how we handle
    coreference resolution.
    
    Previously, we only extracted named entities present in the
    message, but for now we'll keep it simple and extract all
    entities including those in the context.
    
    Coreference resolution is simplified to just storing the
    resolved text as the memory.

--- npcs/memory/nlp.py
+++ npcs/memory/nlp.py
@@ -1,52 +1,28 @@
-from typing import Tuple, Set
-
-import spacy
-from spacy.tokens import Doc
-# we need to import this for the SpacyTextBlob pipeline component to work
+# we need to import these for the pipeline components to work
+# NOTE: the fastcoref component MUST be imported before spacy
+from fastcoref import spacy_component  # noqa # pylint: disable=unused-import
 from spacytextblob.spacytextblob import SpacyTextBlob  # noqa # pylint: disable=unused-import
-
-__nlp = None
-
-
-def default_pipeline() -> spacy.Language:
-    global __nlp
-
-    if __nlp:
-        return __nlp
-
-    __nlp = spacy.load('en_core_web_sm')  # base model, entity extraction
-    __nlp.add_pipe('spacytextblob')  # sentiment analysis
-    __nlp.add_pipe('sentencizer')
-    __nlp.add_pipe('coreferee')  # coreference identification
-    return __nlp
+from spacy.tokens import Doc
+import spacy
 
 
-def entity_extraction(message: str, pipeline: spacy.Language = None, context: str = None) -> Set[str]:
-    """
-    Uses spacy to extract entities from a given message as a comma-separated string.
-    """
-    if not pipeline:
-        pipeline = default_pipeline()
-    doc = pipeline(f'{context}\n{message}')
-    if context:
-        tokenizer = pipeline.tokenizer
-        return _coreference_extraction(doc=doc, tokens_in_context=len(tokenizer(context)))
-    return {str(e) for e in doc.ents}
+class NLPResult:
+    def __init__(self, doc: Doc) -> None:
+        self._doc = doc
+        self.entities = {str(e) for e in doc.ents}
+        self.sentiment_analysis = doc._.blob
+        self.resolved_text = doc._.resolved_text
 
 
-def _coreference_extraction(doc: Doc, tokens_in_context: int) -> any:
-    refs = set()
-    for chain in doc._.coref_chains:
-        # last index of the ref chain is in the message
-        if chain[-1].root_index > tokens_in_context:
-            tokens = {token.text for token in doc._.coref_chains.resolve(doc[chain[-1].root_index])}
-            refs.update(tokens)
-            # TODO: replace unspecific refs (he/him etc.)
-    return refs
+class NLPPipeline:
+    def __init__(self) -> None:
+        nlp = spacy.load('en_core_web_sm')  # base model, entity extraction
+        self._textblob = nlp.add_pipe('spacytextblob')  # sentiment analysis
+        self._coref = nlp.add_pipe('fastcoref')  # coreference resolution
+        self._nlp = nlp
 
-
-def sentiment_analysis(message: str, pipeline: spacy.Language = None) -> Tuple[float, float]:
-    if not pipeline:
-        pipeline = default_pipeline()
-    doc = pipeline(message)
-    return (doc._.blob.polarity, doc._.blob.subjectivity)
+    def run(self, message: str, context: str = None) -> NLPResult:
+        return NLPResult(self._nlp(
+            f'{context}\n{message}',
+            component_cfg={"fastcoref": {'resolve_text': True}}
+        ))
