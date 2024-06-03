60bdfe8f04f3e213d10e20bd64b2062388dbd965
Duncan Sommerville <duncan.sommerville@gmail.com>
2024-06-02 23:18:53 -0400

    Refactor entrypoint

--- npcs/main.py
+++ npcs/main.py
@@ -1,63 +1,52 @@
-from typing import List
+from npcs.memory import Conversation, NPCMemory
 
-from langchain.prompts import PromptTemplate
-from langchain.chains import ConversationChain
-from langchain.memory import ConversationSummaryBufferMemory
-from langchain_community.llms import HuggingFaceEndpoint
-
-from npcs.memory import IndexedMemory
-from npcs.utils.constants import (
-    LLM_TEMP,
-    LLM_FREQUENCY_PENALTY,
-    CONVERSATION_SUMMARY_TOKEN_LIMIT,
-)
-
-TEMPLATE = """The following is a conversation between a Player and an NPC.
-The NPC is wary of the player but not hostile, and will provide the Player with contextual information.
-The NPC ONLY uses information contained in the "Relevant Information" section and does not hallucinate.
-
-Relevant Information:
-
-{history}
-
-Conversation:
-Player: {input}
-NPC:"""
-
-
-class NPC:
-    def __init__(self, template: str, input_vars: List[str]) -> None:
-        self.template = PromptTemplate(
-            template=template,
-            input_variables=input_vars,
-        )
-        self.conversation_chain = ConversationChain(
-            llm=llm,
-            verbose=False,
-            prompt=prompt,
-            memory=IndexedMemory()
-        )
-
-
-if __name__ == '__main__':
-    llm = HuggingFaceEndpoint(
-        repo_id="HuggingFaceH4/zephyr-7b-beta",
-        temperature=LLM_TEMP,
-        repetition_penalty=LLM_FREQUENCY_PENALTY,
+# const values for testing the LLM
+NPC_NAME = "Jimmy Buffett"
+SEED_MEMORIES = [
+    NPCMemory(
+        npc="Jimmy Buffett",
+        memory="A mug of ale costs 4 copper pieces.",
+        entities={"ale"},
+    ),
+    NPCMemory(
+        npc="Jimmy Buffett",
+        memory="This inn's name is the Silver Fox.",
+        entities={"Silver Fox"},
+    ),
+    NPCMemory(
+        npc="Jimmy Buffett",
+        memory="It costs 1 silver piece a night to stay at the inn.",
+        entities={"Silver Fox"},
+    ),
+    NPCMemory(
+        npc="Jimmy Buffett",
+        memory="A mug of ale costs 4 copper pieces at the Silver Fox.",
+        entities={"ale", "Silver Fox"},
     )
-    prompt = PromptTemplate(template=TEMPLATE, input_variables=["history", "input"])
-    convo = ConversationChain(
-        llm=llm,
-        verbose=False,
-        prompt=prompt,
-        memory=ConversationSummaryBufferMemory(
-            llm=llm,
-            human_prefix='Player',
-            ai_prefix='NPC',
-            max_token_limit=CONVERSATION_SUMMARY_TOKEN_LIMIT,
-        ),
-    )
+]
+
+
+# pre-seed the index with some memories
+def seed_index():
+    from npcs.memory.search import default_index, search_memories
+    idx = default_index()
+    old_memories = search_memories(idx, f'npc:"{NPC_NAME}"')
+    for memory in old_memories:
+        print(memory.memory)
+    w = idx.writer()
+    w.delete_by_term("npc", NPC_NAME)
+    for memory in SEED_MEMORIES:
+        w.add_document(**memory.as_dict())
+    w.commit()
+
+
+def run():
+    seed_index()
+    convo = Conversation(name=NPC_NAME)
 
     while True:
-        inp = input("Prompt: ")
-        print(convo.predict(input=inp))
+        message = input("Prompt: ")
+        print(convo.say(message))
+
+if __name__ == '__main__':
+    run()
