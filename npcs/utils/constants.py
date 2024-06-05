c37cf3d5a3947c415d926a29e2ac63f0b76dfb76
Duncan Sommerville <duncan.sommerville@gmail.com>
2024-06-04 23:50:16 -0400

    Move index consts to utils

--- npcs/utils/constants.py
+++ npcs/utils/constants.py
@@ -11,8 +11,11 @@ LLM_FREQUENCY_PENALTY = 1.0
 
 CONVERSATION_SUMMARY_TOKEN_LIMIT = 2048
 
+NPC_INDEX_NAME = "npc_memories"
+
 # FIXME: change this to a function, e.g. get_index_dir
 # that works x-platform (%APPDATA% on Windows) and allows
 # the user to specify a custom path.
 HOME_DIRECTORY = Path.home()
 ROOT_DIRECTORY = HOME_DIRECTORY / ".config" / "npcs"
+INDEX_PATH = ROOT_DIRECTORY / "index"
