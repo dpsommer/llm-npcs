b7dc586a0e5268c316e34809605292a11d6a199e
Duncan Sommerville <duncan.sommerville@gmail.com>
2024-06-04 23:53:17 -0400

    Remove Whoosh schema

--- npcs/memory/__init__.py
+++ npcs/memory/__init__.py
@@ -1,14 +1,13 @@
 from .conversation import Conversation
 from .index import IndexedMemory
 from .nlp import NLPPipeline
-from .schema import NPCMemory, NPCMemorySchema
+from .schema import NPCMemory
 from .search import FAISSVectorStore
 
 __all__ = [
     "Conversation",
     "FAISSVectorStore",
     "IndexedMemory",
-    "NPCMemorySchema",
     "NPCMemory",
     "NLPPipeline",
 ]
--- npcs/memory/schema.py
+++ npcs/memory/schema.py
@@ -1,19 +1,6 @@
 from dataclasses import asdict, dataclass, field
 from typing import Set
 
-from whoosh.fields import ID, KEYWORD, NUMERIC, TEXT, SchemaClass
-
-
-# is it better to store individual memories or whole conversations?
-class NPCMemorySchema(SchemaClass):
-    npc = ID(stored=True)
-    # other characters involved in the memory
-    characters = KEYWORD(commas=True)
-    memory = TEXT(stored=True)
-    entities = KEYWORD(commas=True, scorable=True)
-    sentiment_polarity = NUMERIC
-    sentiment_subjectivity = NUMERIC
-
 
 @dataclass
 class NPCMemory:
